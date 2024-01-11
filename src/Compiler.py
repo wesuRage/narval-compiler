fb5f55361ba95e58f85707007e32d3b2be9923a5
wesuRage <weslley.nmiranda@gmail.com>
2024-01-11 13:57:07 -0300

    set to linux

--- src/Compiler.py
+++ src/Compiler.py
@@ -4,6 +4,6 @@ class Compiler:
   def compile(self, output):
     out = output.split(".")[0]
     system(f"nasm -felf64 {out}.asm -o {out}.o")
-    system(f"gcc -no-pie -o {out} -z noexecutestack {out}.o")
+    system(f"gcc -no-pie -o {out} -z noexecstack {out}.o")
     # system(f"rm {out}.asm")
     system(f"rm {out}.o")
