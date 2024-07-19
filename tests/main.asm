4a6be05986bdb98da788bf24c6afd3792b93f761
wesuRage <weslley.nmiranda@gmail.com>
2024-07-19 14:40:00 -0300

    better generation

--- tests/main.asm
+++ tests/main.asm
@@ -4,23 +4,16 @@ entry main
 include "/root/rust/narval/libs/standard.s"
 
 segment readable writeable
-	expr dq 0
-	str_ dq 0x0
+	expr dq "sexo", 0x0
+	txt dq 0x0
 
 segment readable executable
 main:
-	mov rax, 9
-	mov rbx, rax
-	mov rax, 2
-	xchg rax, rbx
-	cqo
-	idiv rbx
-	mov [expr], rax
-	push qword [expr]
-	call int_to_str
+	push expr
+	call totxt
 
-	mov [str_], rax
-	push qword [str_]
+	mov [txt], rax
+	push qword [txt]
 	call write
 
 	push 0
