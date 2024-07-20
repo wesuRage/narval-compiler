ccbfe70e14ae0c08e6f59a6e597a713a407dbe8b
wesuRage <weslley.nmiranda@gmail.com>
2024-07-20 19:17:55 -0300

    fixed installation

--- install.sh
+++ install.sh
@@ -1,4 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+
+[ "$UID" -ne 0 ] &&  echo "Run the script as root."; exit 1
+
+
+if [ -z ${NARVAL_HOME+x} ]; then
+    NARVAL_HOME=$(pwd)
+    echo -e "\nNARVAL_HOME=$NARVAL_HOME" >> /etc/bash.bashrc
+    . /etc/bash.bashrc
+fi
 
 cp tools/fasm /bin/basm
 
