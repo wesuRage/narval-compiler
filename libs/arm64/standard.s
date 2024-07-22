0ead3475aef19ff32f966a17039a844202b81917
wesuRage <weslley.nmiranda@gmail.com>
2024-07-21 23:00:02 -0300

    transcripted some x86_64 functions to arm64

--- libs/arm64/standard.s
+++ libs/arm64/standard.s
@@ -10,6 +10,25 @@
         .asciz "[2J"
 .text
 //--------------------------------------------------
+__pow:
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
+
+    mov x14, x1
+    mov x15, x0
+    mov x16, x14
+    mov x14, #1
+    cbz x16, .pow_end
+.pow_loop:
+    mul x14, x14, x15
+    sub x16, x16, #1
+    cbnz x16, .pow_loop
+.pow_end:
+    mov x8, x14
+    ldp x29, x30, [sp], #16
+    ret
+
+//--------------------------------------------------
 clear:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
@@ -50,11 +69,67 @@ exit:
     ret
 
 //--------------------------------------------------
+read:
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
+
+    mov x14, x1
+    mov x15, x0
+
+    mov x0, x15
+    bl len
+    mov x2, x8
+
+    mov x0, STD_IN
+    mov x1, x14
+    mov x8, SYS_read
+    svc #0
+
+    ldp x29, x30, [sp], #16
+    ret
+
+//--------------------------------------------------
+write_raw:
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
+
+    mov x14, x2
+    mov x15, x1
+    mov x16, x0
+
+    mov x0, x16
+    mov x1, x15
+    mov x2, x14
+    mov x8, SYS_write
+    svc #0
+    
+    ldp x29, x30, [sp], #16
+    ret
+
+//--------------------------------------------------
+open:
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
+
+    mov x14, x2
+    mov x15, x1
+    mov x16, x0
+
+    mov x0, x16
+    mov x1, x15
+    mov x2, x14
+    mov x8, SYS_open
+
+    ldp x29, x30, [sp], #16
+    ret
+
+//--------------------------------------------------
 write:
     stp x29, x30, [sp, #-16]!
     mov x29, sp
 
     mov x14, x0
+    mov x0, x14
     bl len
 
     mov x0, STD_OUT
@@ -90,6 +165,23 @@ len:
 
 .done:
     mov x8, x15
+
+    ldp x29, x30, [sp], #16
+    ret
+
+//--------------------------------------------------
+
+// TODO: Finish this function
+totxt:
+    stp x29, x30, [sp, #-16]!
+    mov x29, sp
+
+    mov x14, x0
+    mov x15, #10
+    udiv x16, x14, x15
+    msub x17, x16, x15, x14
+    add w17, w17, #'0'
+    mov x8, x17
 
     ldp x29, x30, [sp], #16
     ret
