875c8ad544b4783e8d4dc8c27bb607c785edc088
wesuRage <weslley.nmiranda@gmail.com>
2024-08-02 00:53:32 -0300

    misc(dev): dev tests

--- tests/main.asm
+++ tests/main.asm
@@ -4,76 +4,23 @@ entry main
 include "/var/lib/narval/libs/x86_64/standard.s"
 
 segment readable writeable
-	slashe_targe dq 0
-	slashe_niggatron dq 0
-	__TEMP_RETURN_0 dq 0
-	resto_rest dq 0
-	__TEMP_RETURN_1 dq 0
-	num dq 0x0
-	string dq 0x0
+	__STR_0 db "1", 0x0
+	pinto dq 0x0
+	rola dq 0x0
 
 segment readable executable
 main:
-	push 9
-	call slashe
+	mov rdi, __STR_0
+	call toint
 
-	mov [num], rax
-	push qword [num]
+	mov [pinto], rax
+	mov rdi, [pinto]
 	call totxt
 
-	mov [string], rax
-	push qword [string]
+	mov [rola], rax
+	mov rdi, [rola]
 	call write
 
-	push 0
+	mov rdi, 0
 	call exit
-
-slashe:
-	push rbp
-	mov rbp, rsp
-
-	mov rdi, [rbp+16]
-	mov rax, 2
-	mov rbx, rax
-	mov rax, rdi
-	add rax, rbx
-	mov [slashe_targe], rax
-	mov rax, [slashe_targe]
-	mov rbx, rax
-	mov rax, 3
-	push rax
-	push rbx
-	call __pow
-
-	mov [slashe_niggatron], rax
-	push qword [slashe_niggatron]
-	call resto
-
-	mov [__TEMP_RETURN_0], rax
-	mov rax, [__TEMP_RETURN_0]
-
-	mov rsp, rbp
-	pop rbp
-	ret
-
-resto:
-	push rbp
-	mov rbp, rsp
-
-	mov rdi, [rbp+16]
-	mov rax, rdi
-	mov rbx, rax
-	mov rax, 4
-	xchg rax, rbx
-	cqo
-	idiv rbx
-	mov rax, rdx
-	mov [resto_rest], rax
-	mov rax, [resto_rest]
-	mov [__TEMP_RETURN_1], rax
-	mov rax, [__TEMP_RETURN_1]
-
-	mov rsp, rbp
-	pop rbp
-	ret
 
