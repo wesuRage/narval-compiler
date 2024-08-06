35a9d519ffdc40f4198be21f4ea8a8e1a35e45d7
Alexandre Barreto <121456361+IfLostart2per5@users.noreply.github.com>
2024-08-06 15:50:40 +0000

    .2
    yes

--- tests/main.asm
+++ tests/main.asm
@@ -4,32 +4,16 @@ entry main
 include "/var/lib/narval/libs/x86_64/standard.s"
 
 segment readable writeable
-	__INT_0_PTR db 3
-	pinto db 0, __INT_0_PTR
-	__STR_0_PTR db "oi"
-	__STR_0 dq 2, __STR_0_PTR
-	__STR_1_PTR db "chora paulin"
-	__STR_1 dq 2, __STR_1_PTR
+	pinto db 50
+	pintos db 0x0
 
 segment readable executable
 main:
-__if_0:
-	mov rax, 1
-	mov rbx, rax
-	mov rax, 1
-	cmp rax, rbx
-	jne __if_0_consequent_0
-	mov rdi, __STR_0
-	call write
+	mov rdi, pinto
+	call totxt
 
-__if_alternate_0:
-__if_0:
-	mov rax, 2
-	mov rbx, rax
-	mov rax, 3
-	cmp rax, rbx
-	jne __if_0_consequent_1
-	mov rdi, __STR_1
+	mov [pintos], al
+	movzx rdi, byte [pintos]
 	call write
 
 __end_if_0:
