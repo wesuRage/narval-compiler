4c06711f0d219fdffeeaf329102d5a2728d1a991
wesuRage <weslley.nmiranda@gmail.com>
2024-08-05 23:07:14 -0300

    misc(dev): dev tests

--- tests/main.asm
+++ tests/main.asm
@@ -5,57 +5,35 @@ include "/var/lib/narval/libs/x86_64/standard.s"
 
 segment readable writeable
 	__INT_0_PTR db 3
-	num dq 0, __INT_0_PTR
-	numero dq 0, 0
-	hashtag dq 2, 0
-	__STR_0_PTR db "#", 0
+	pinto db 0, __INT_0_PTR
+	__STR_0_PTR db "oi"
 	__STR_0 dq 2, __STR_0_PTR
-	total dq 2, 0
-	__STR_1_PTR db "total:"
+	__STR_1_PTR db "chora paulin"
 	__STR_1 dq 2, __STR_1_PTR
 
 segment readable executable
 main:
-	mov rax, [num+8]
-	mov rax, [rax]
-	add rax, rax
-	mov [numero+8], rax
+__if_0:
+	mov rax, 1
 	mov rbx, rax
 	mov rax, 1
-	add rax, rax
-	mov [numero+8], rax
-	mov rdi, rbx
-	mov rsi, rax
-	call __pow
-	mov [numero+8], rax
-	mov rbx, rax
-	mov rax, 2
-	add rax, rbx
-	mov [numero+8], rax
-	mov rbx, rax
-	mov rax, 2
-	xchg rax, rbx
-	cqo
-	idiv rbx
-	mov [numero+8], rax
-	mov rax, [numero+8]
-	mov rdi, rax
-	mov rsi, [__STR_0+8]
-	call __txt_repeater
-	mov [hashtag+8], rax
-	mov rdi, numero
-	call totxt
-
-	mov [total+8], rax
-	mov rdi, hashtag
+	cmp rax, rbx
+	jne __if_0_consequent_0
+	mov rdi, __STR_0
 	call write
 
+__if_alternate_0:
+__if_0:
+	mov rax, 2
+	mov rbx, rax
+	mov rax, 3
+	cmp rax, rbx
+	jne __if_0_consequent_1
 	mov rdi, __STR_1
 	call write
 
-	mov rdi, total
-	call write
-
+__end_if_0:
+__end_if_1:
 	mov rdi, 0
 	call exit
 
